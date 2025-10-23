84ebb199d38dd710b40d8070bed2f30b7dc3ae9e
nathapatt <nathapat.mind@gmail.com>
2025-10-24 04:53:02 +0700

    add Socket.IO proxy configuration and update socket connection URL

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -78,7 +78,7 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
 
     setConnecting(true);
 
-    const newSocket = io(`${import.meta.env.VITE_NGINX_PROXY}`, {
+    const newSocket = io(window.location.origin, {
       withCredentials: true,
       transports: ["websocket", "polling"],
       autoConnect: true,
